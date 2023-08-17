f7381c5a618c59e968043d3f970c4e9d1c60523a
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-08-17 04:00:17 +0200

    Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { LightModeOutlined, DarkModeOutlined, Menu as MenuIcon, Search, SettingsOutlined, ArrowDropDownOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
