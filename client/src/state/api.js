9ab710e957c5eb185128dbcb6760c996c1684db8
Mandile Andile Marotya <marotyamandile@gmail.com>
2023-09-12 22:43:50 +0200

    Sales Overview

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -9,7 +9,7 @@ export const api = createApi({
         "Customers",
         "Transactions",
         "Geography",
-        //"Sales",
+        "Sales",
         //"Admins",
         //"Performance",
         //"Dashboard",
@@ -41,6 +41,10 @@ export const api = createApi({
             query: () => "client/geography",
             providesTags: ["Geography"],
         }),
+        getSales: build.query({
+            query: () => "sales/sales",
+            providesTags: ["Sales"],
+          }),
     })
 })
 
@@ -50,4 +54,5 @@ export const {
     useGetCustomersQuery,
     useGetTransactionsQuery,
     useGetGeographyQuery,
+    useGetSalesQuery,
 } = api;
